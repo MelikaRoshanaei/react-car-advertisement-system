fc06b4950c2b123ee07be3b4e67d688b761917e3
MelikaRoshanaei <MelikaRoshanaei2003@gmail.com>
2025-06-25 23:20:47 +0430

    feat: add a simple database test connection

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import dotenv from "dotenv";
 import express from "express";
+import pool from "./db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+// Test DB connection
+(async () => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT NOW()");
+    console.log("✅ DB connected successfully!");
+    console.log("Current time:", result.rows[0].now);
+    client.release();
+  } catch (err) {
+    console.error("❌ DB connection failed:", err.message);
+    process.exit(1); // Stop the app if DB fails
+  }
+})();
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
