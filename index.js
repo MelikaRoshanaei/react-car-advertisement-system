6a24172e5b10344272ff551b99610a9f116698fa
MelikaRoshanaei <MelikaRoshanaei2003@gmail.com>
2025-07-12 02:00:50 +0430

    feat: add GET /users/:id route to fetch a single user from database

--- index.js
+++ index.js
@@ -805,6 +805,27 @@ app.get("/users", async (req, res) => {
   }
 });
 
+app.get("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const client = await pool.connect();
+  try {
+    const result = await client.query("SELECT * FROM users WHERE id = $1", [
+      id,
+    ]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "User Not Found!" });
+    }
+
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    console.log(`Error Fetching User With ID ${id}:`, err.message);
+    res.status(500).json({ error: "Failed To Get User Details!" });
+  } finally {
+    client.release();
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
