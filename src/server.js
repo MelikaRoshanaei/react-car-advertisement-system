eca73d5669cb95785385ab9d4a989ec7206b6926
MelikaRoshanaei <MelikaRoshanaei2003@gmail.com>
2025-07-14 21:01:31 +0430

    refactor: create server.js with Express setup and database connection

---
+++ src/server.js
@@ -0,0 +1,44 @@
+import dotenv from "dotenv";
+import express from "express";
+import pool from "./config/db.js";
+import carsRoutes from "./routes/carsRoutes.js";
+import usersRoutes from "./routes/usersRoutes.js";
+import errorHandler from "./utils/errorHandler.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Middleware
+app.use(express.json());
+
+// Test DB Connection
+(async () => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT NOW()");
+    console.log("✅ DB Connected Successfully!");
+    console.log("Current Time:", result.rows[0].now);
+    client.release();
+  } catch (err) {
+    console.error("❌ DB Connection Failed:", err.message);
+    process.exit(1); // Stop the app if DB fails
+  }
+})();
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Root Route Placeholder!");
+});
+app.use("/api/cars", carsRoutes);
+app.use("/api/users", usersRoutes);
+
+// Error Handling
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
+
+export default app;
